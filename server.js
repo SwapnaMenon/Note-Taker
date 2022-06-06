31e1b56a122d474c729ecf813cc1a55822e6623b
Swapna <swapnamenon8@gmail.com>
2022-06-06 17:21:14 -0400

    displying paqes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express= require ("express")
 const path = require ("path")
 const data = require ("./db/db.json")
 const fs = require ("fs")
+const { pathToFileURL } = require("url")
 const app = express() 
 
 const PORT = process.env.PORT || 3001 //creating express-mandatory//
@@ -11,7 +12,12 @@ app.use (express.static("public"))
 app.use (express.urlencoded({extended:true})) //allow front end and backend communication//
 
 
-
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname,"./public/index.html"))
+})
+app.get("/notes",function(req,res){
+    res.sendFile(path.join(__dirname,"./public/notes.html"))
+})
 
 
 app.listen(PORT,function(){
